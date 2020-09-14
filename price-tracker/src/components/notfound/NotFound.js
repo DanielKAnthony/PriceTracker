b9494dae54f00bd59c74eef103f91773bdd3f5c5
DanielKAnthony <danielkrohnanthony@gmail.com>
2020-09-14 14:43:45 -0400

    UI edit

--- price-tracker/src/components/notfound/NotFound.js
+++ price-tracker/src/components/notfound/NotFound.js
@@ -4,9 +4,9 @@ export default class NotFound extends Component {
     render() {
         return (
             <div>
-            <h1 style={{ color: "blue" }}>Page Not Found</h1>
+                <h1 style={{ color: "blue" }}>Page Not Found</h1>
                 <h4>Try another link.</h4>
-                </div>
+            </div>
         );
     }
 }
