8867dec219867d7a9e9a1bf52b9352c2e7f62b2e
DanielKAnthony <danielkrohnanthony@gmail.com>
2020-09-15 12:16:16 -0400

    Cloud migration

---
+++ Migrations/20200915152557_CloudPushInit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PriceTrackerApp.Migrations
+{
+    public partial class CloudPushInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
